9036dcf21cd8536bdac75f7479fc1d8c7d60c59a
Keaton <keatoncs@yahoo.com>
2024-11-01 16:28:20 -0500

    minor UI updates

--- obj/Debug/net8.0/faceitWebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/faceitWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("faceitWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+924781e3d96e3ba3e1eea21fd23b8f72b04d7525")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f5f8f4f161f0ca78b9ca7d3da1059bfd0eeaac6")]
 [assembly: System.Reflection.AssemblyProductAttribute("faceitWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("faceitWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
