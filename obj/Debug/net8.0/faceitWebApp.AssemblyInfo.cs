b1c69addd0f9febe0b4232b9b08e64b6ebf2a509
Keaton <keatoncs@yahoo.com>
2024-11-04 15:49:29 -0600

    icon color change and minor bug fixes

--- obj/Debug/net8.0/faceitWebApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/faceitWebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("faceitWebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db0c1a67714a1df2c1d7575d30549b140df75fd3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e200d223d0b260a75614bd7aaa3ee13d963f55ee")]
 [assembly: System.Reflection.AssemblyProductAttribute("faceitWebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("faceitWebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
