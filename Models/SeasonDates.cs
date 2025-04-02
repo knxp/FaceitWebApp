ee9c9943595bd25d066d7a86a915735168cde603
Keaton <keatoncollatt@yahoo.com>
2025-04-02 12:47:47 -0500

    added S53

--- Models/SeasonDates.cs
+++ Models/SeasonDates.cs
@@ -3,13 +3,15 @@ public class SeasonDates
     public static readonly Dictionary<string, (long Start, long End)> Seasons = new()
     {
         { "ESEA S51", (1728190800, 1734328800) },
-        { "ESEA S52", (1736748000, 1742706000) }
+        { "ESEA S52", (1736748000, 1742706000) },
+        { "ESEA S53", (1743897600, 1747267200) }
     };
 
     public static readonly Dictionary<string, string[]> MapPools = new()
     {
         { "ESEA S51", new[] { "Ancient", "Anubis", "Inferno", "Mirage", "Nuke", "Vertigo", "Dust2" } },
-        { "ESEA S52", new[] { "Ancient", "Anubis", "Inferno", "Mirage", "Nuke", "Train", "Dust2" } }
+        { "ESEA S52", new[] { "Ancient", "Anubis", "Inferno", "Mirage", "Nuke", "Train", "Dust2" } },
+        { "ESEA S53", new[] { "Ancient", "Anubis", "Inferno", "Mirage", "Nuke", "Train", "Dust2" } }
     };
 
     public static bool IsMatchInSeason(long matchTimestamp, string competitionName)
